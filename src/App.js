1bd1e04c75c5cd692e170a8e1d886205626dcdb3
Matthew Minor <mattsminor@gmail.com>
2022-06-07 23:19:01 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
+import {config} from './config';
 
-function App() {
+function randomDateGen(start, end){
+  return new Date(
+        start.getTime() +
+        Math.random() *
+        (end.getTime() -
+        start.getTime()));
+}
+
+export default function App(){
+  const [pic, setPic] = useState();
+  const [picName, setPicName] = useState('');
+  const [picExplanation, setPicExplanation] = useState('');
+
+
+  const fetchPicOfDay = async () => {
+    const apiKey = config.MY_KEY;
+    
+    // 1995-06-16
+    // Create a new date later than the above
+    // and no later that today.
+    // append to the url like &date=2014-10-01
+    const randomDate = randomDateGen(new Date(1995, 6, 16), new Date()).toISOString().slice(0, 10);
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${randomDate}`;
+    console.log(url);
+    const res = await fetch(url)
+    const json = await res.json();
+    console.log(json);
+    setPic(json.url);
+    setPicName(json.title);
+    setPicExplanation(json.explanation);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="LoadDataButton" onClick={fetchPicOfDay}>Load Pic Data</button>
+      <h2 className="PicTitle">{picName}</h2>
+      <img 
+      src={pic}
+      alt="new"/>
+
+      <p className="PicDescription">
+        {picExplanation}
+      </p>
     </div>
   );
 }
-
-export default App;
